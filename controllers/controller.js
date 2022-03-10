ca16c8fa1723b538f9c34ca4ee11cf7af4062297
amontalvof <amontalvof92@gmail.com>
2022-03-10 08:45:36 -0600

    feat: Two way data binding and ng-model

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,13 @@
 const app = angular.module('MyFirstApp', []);
 
 app.controller('FirstController', function ($scope) {
-    $scope.name = 'Andy';
+    $scope.newComment = {};
+    $scope.comments = [
+        { comment: 'Good', username: 'Homer' },
+        { comment: 'Bad', username: 'Lisa' },
+    ];
+    $scope.addComment = function () {
+        $scope.comments.push($scope.newComment);
+        $scope.newComment = {};
+    };
 });
