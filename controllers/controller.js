e04fb1b6957f51bc73a68a17fd927568b0dc6c91
amontalvof <amontalvof92@gmail.com>
2022-03-11 09:01:25 -0600

    feat: Dependency Injection

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,17 @@
 const app = angular.module('MyFirstApp', []);
 
-app.controller('FirstController', function ($scope) {
-    $scope.newComment = {};
-    $scope.comments = [
-        { comment: 'Good', username: 'Homer' },
-        { comment: 'Bad', username: 'Lisa' },
-    ];
-    $scope.addComment = function () {
-        $scope.comments.push($scope.newComment);
+app.controller('FirstController', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
         $scope.newComment = {};
-    };
-});
+        $scope.comments = [
+            { comment: 'Good', username: 'Homer' },
+            { comment: 'Bad', username: 'Lisa' },
+        ];
+        $scope.addComment = function () {
+            $scope.comments.push($scope.newComment);
+            $scope.newComment = {};
+        };
+    },
+]);
