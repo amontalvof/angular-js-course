3579c49fb1b8a6836d55e9d7d1433a96c99e7ddd
amontalvof <amontalvof92@gmail.com>
2022-04-04 18:24:43 -0500

    feat: Factories

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,15 +1,49 @@
-const app = angular.module('MyFirstApp', []).run(function ($rootScope) {
-    $rootScope.name = 'Hello world';
-});
+const app = angular.module('ToDoList', ['LocalStorageModule']);
 
-app.controller('FirstController', [
+app.factory('ToDoService', function (localStorageService) {
+    const todoService = {
+        key: 'angular-todoList',
+        todos: [],
+    };
+    if (localStorageService.get(todoService.key)) {
+        todoService.todos = localStorageService.get(todoService.key);
+    } else {
+        todoService.todos = [];
+    }
+    todoService.add = function (newTodo) {
+        todoService.todos.unshift(newTodo);
+        todoService.updateLocalStorage();
+    };
+    todoService.updateLocalStorage = function () {
+        localStorageService.set(todoService.key, todoService.todos);
+    };
+    todoService.clean = function () {
+        todoService.todos = [];
+        todoService.updateLocalStorage();
+    };
+    todoService.getAll = function () {
+        return todoService.todos;
+    };
+    todoService.removeItem = function (item) {
+        todoService.todos = todoService.todos.filter((todo) => todo !== item);
+        todoService.updateLocalStorage();
+        return todoService.getAll();
+    };
+    return todoService;
+}).controller('ToDoController', [
     '$scope',
-    function ($scope) {
-        $scope.name = 'lorem';
-        setTimeout(function () {
-            $scope.$apply(function () {
-                $scope.name = 'ipsum';
-            });
-        }, 1000);
+    'ToDoService',
+    function ($scope, ToDoService) {
+        $scope.todos = ToDoService.getAll();
+        $scope.addTodo = function () {
+            ToDoService.add($scope.newTodo);
+            $scope.newTodo = {};
+        };
+        $scope.removeTodo = function (item) {
+            $scope.todos = ToDoService.removeItem(item);
+        };
+        $scope.clearTodos = function () {
+            $scope.todos = ToDoService.clean();
+        };
     },
-]).controller('ChildController', ['$scope', function ($scope) {}]);
+]);
