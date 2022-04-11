14cfc5d5d2fbc7a500f649234d02b0fff9e20191
amontalvof <amontalvof92@gmail.com>
2022-04-11 10:52:22 -0500

    feat: ng-src and ng-cloak

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,46 +1,16 @@
-const app = angular.module('ToDoList', ['LocalStorageModule']);
+const app = angular.module('CustomDirective', []);
 
-app.service('ToDoService', function (localStorageService) {
-    this.key='angular-todoList'
-    this.todos=[]
-    if (localStorageService.get(this.key)) {
-        this.todos = localStorageService.get(this.key);
-    } else {
-        this.todos = [];
-    }
-    this.add = function (newTodo) {
-        this.todos.unshift(newTodo);
-        this.updateLocalStorage();
-    };
-    this.updateLocalStorage = function () {
-        localStorageService.set(this.key, this.todos);
-    };
-    this.clean = function () {
-        this.todos = [];
-        this.updateLocalStorage();
-    };
-    this.getAll = function () {
-        return this.todos;
-    };
-    this.removeItem = function (item) {
-        this.todos = this.todos.filter((todo) => todo !== item);
-        this.updateLocalStorage();
-        return this.getAll();
-    };
-}).controller('ToDoController', [
+app.controller('AppCtrl', [
     '$scope',
-    'ToDoService',
-    function ($scope, ToDoService) {
-        $scope.todos = ToDoService.getAll();
-        $scope.addTodo = function () {
-            ToDoService.add($scope.newTodo);
-            $scope.newTodo = {};
-        };
-        $scope.removeTodo = function (item) {
-            $scope.todos = ToDoService.removeItem(item);
-        };
-        $scope.clearTodos = function () {
-            $scope.todos = ToDoService.clean();
-        };
+    '$http',
+    function ($scope, $http) {
+        $http
+            .get('https://api.github.com/users/amontalvof/repos')
+            .then(function (response) {
+                $scope.repos = response.data;
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     },
 ]);
