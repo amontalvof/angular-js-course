248ed042e9f5bf61c99582640afabcb2cc0f9258
amontalvof <amontalvof92@gmail.com>
2022-04-12 10:14:13 -0500

    feat: Create Directives part 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,16 +1,22 @@
 const app = angular.module('CustomDirective', []);
 
-app.controller('AppCtrl', [
-    '$scope',
-    '$http',
-    function ($scope, $http) {
-        $http
-            .get('https://api.github.com/users/amontalvof/repos')
-            .then(function (response) {
-                $scope.repos = response.data;
-            })
-            .catch(function (err) {
-                console.log(err);
+app.directive('backImg', function () {
+    return function (scope, element, attrs) {
+        attrs.$observe('backImg', function (value) {
+            element.css({
+                background: 'url(' + value + ')',
+                'background-size': 'cover',
+                'background-position': 'center',
             });
-    },
-]);
+        });
+    };
+}).controller('AppCtrl', function ($scope, $http) {
+    $http
+        .get('https://api.github.com/users/amontalvof/repos')
+        .then(function (response) {
+            $scope.repos = response.data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+});
