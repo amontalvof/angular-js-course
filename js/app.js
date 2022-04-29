4241e878d10ef569cf98e4b9bde522a10ce5690c
amontalvof <amontalvof92@gmail.com>
2022-04-28 20:39:13 -0500

    feat: Final Practice 3/4

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 angular
     .module('FinalApp', ['lumx', 'ngRoute', 'ngResource'])
     .config(function ($routeProvider) {
-        $routeProvider.when('/', {
-            controller: 'MainController',
-            templateUrl: 'templates/home.html',
-        });
+        $routeProvider
+            .when('/', {
+                controller: 'MainController',
+                templateUrl: 'templates/home.html',
+            })
+            .when('/post/:id', {
+                controller: 'PostController',
+                templateUrl: 'templates/post.html',
+            });
     });
--- js/controllers.js
+++ js/controllers.js
@@ -9,4 +9,17 @@ angular
         });
         $scope.posts = Post.query();
         $scope.users = User.query();
+
+        $scope.removePost = function (post) {
+            Post.delete({ id: post.id }, function (data) {
+                console.log(data);
+            });
+            $scope.posts = $scope.posts.filter((item) => item.id !== post.id);
+        };
+    })
+    .controller('PostController', function ($scope, $routeParams, $resource) {
+        Post = $resource('http://jsonplaceholder.typicode.com/posts/:id', {
+            id: '@id',
+        });
+        $scope.post = Post.get({ id: $routeParams.id });
     });
